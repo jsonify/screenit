accad6f458805469bae24e9d47503a2a158730d4
jsonify <phlacin@gmail.com>
2025-07-14 17:37:46 -0700

    Initial Commit

---
+++ screenit/screenitApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenitApp.swift
+//  screenit
+//
+//  Created by Jason Rueckert on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct screenitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
